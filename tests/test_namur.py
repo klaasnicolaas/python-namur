16f8ef1f6a68bb72e0f6c301e4dbf636910e93f9
Klaas Schoute <klaas_schoute@hotmail.com>
2023-03-13 17:27:58 +0100

    General refactor/restructure of project

--- tests/test_namur.py
+++ tests/test_namur.py
@@ -3,8 +3,8 @@
 import asyncio
 from unittest.mock import patch
 
-import aiohttp
 import pytest
+from aiohttp import ClientError, ClientResponse, ClientSession
 from aresponses import Response, ResponsesMockServer
 
 from namur import ODPNamur
@@ -13,7 +13,6 @@ from namur.exceptions import ODPNamurConnectionError, ODPNamurError
 from . import load_fixtures
 
 
-@pytest.mark.asyncio
 async def test_json_request(aresponses: ResponsesMockServer) -> None:
     """Test JSON response is handled correctly."""
     aresponses.add(
@@ -26,14 +25,13 @@ async def test_json_request(aresponses: ResponsesMockServer) -> None:
             text=load_fixtures("parking_pmr.json"),
         ),
     )
-    async with aiohttp.ClientSession() as session:
+    async with ClientSession() as session:
         client = ODPNamur(session=session)
         response = await client._request("test")
         assert response is not None
         await client.close()
 
 
-@pytest.mark.asyncio
 async def test_internal_session(aresponses: ResponsesMockServer) -> None:
     """Test internal session is handled correctly."""
     aresponses.add(
@@ -50,25 +48,24 @@ async def test_internal_session(aresponses: ResponsesMockServer) -> None:
         await client._request("test")
 
 
-@pytest.mark.asyncio
 async def test_timeout(aresponses: ResponsesMockServer) -> None:
     """Test request timeout is handled correctly."""
     # Faking a timeout by sleeping
-    async def reponse_handler(_: aiohttp.ClientResponse) -> Response:
+    async def reponse_handler(_: ClientResponse) -> Response:
         await asyncio.sleep(0.2)
         return aresponses.Response(
-            body="Goodmorning!", text=load_fixtures("parking_pmr.json")
+            body="Goodmorning!",
+            text=load_fixtures("parking_pmr.json"),
         )
 
     aresponses.add("data.namur.be", "/api/records/1.0/test", "GET", reponse_handler)
 
-    async with aiohttp.ClientSession() as session:
+    async with ClientSession() as session:
         client = ODPNamur(session=session, request_timeout=0.1)
         with pytest.raises(ODPNamurConnectionError):
             assert await client._request("test")
 
 
-@pytest.mark.asyncio
 async def test_content_type(aresponses: ResponsesMockServer) -> None:
     """Test request content type error is handled correctly."""
     aresponses.add(
@@ -81,7 +78,7 @@ async def test_content_type(aresponses: ResponsesMockServer) -> None:
         ),
     )
 
-    async with aiohttp.ClientSession() as session:
+    async with ClientSession() as session:
         client = ODPNamur(
             session=session,
         )
@@ -89,12 +86,13 @@ async def test_content_type(aresponses: ResponsesMockServer) -> None:
             assert await client._request("test")
 
 
-@pytest.mark.asyncio
 async def test_client_error() -> None:
     """Test request client error is handled correctly."""
-    async with aiohttp.ClientSession() as session:
+    async with ClientSession() as session:
         client = ODPNamur(session=session)
         with patch.object(
-            session, "request", side_effect=aiohttp.ClientError
+            session,
+            "request",
+            side_effect=ClientError,
         ), pytest.raises(ODPNamurConnectionError):
             assert await client._request("test")
