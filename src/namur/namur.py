26e97f7e2b857f8e21d20d4e18a127a13bb7bf13
Klaas Schoute <klaas_schoute@hotmail.com>
2023-12-16 04:33:00 +0100

    Fix import and update async timeout

--- src/namur/namur.py
+++ src/namur/namur.py
@@ -5,9 +5,8 @@ import asyncio
 import socket
 from dataclasses import dataclass
 from importlib import metadata
-from typing import TYPE_CHECKING, Any, cast
+from typing import Any, Self, cast
 
-import async_timeout
 from aiohttp import ClientError, ClientSession
 from aiohttp.hdrs import METH_GET
 from yarl import URL
@@ -19,9 +18,6 @@ from .exceptions import (
     ODPNamurTypeError,
 )
 from .models import ParkingSpot
-
-if TYPE_CHECKING:
-    from typing_extensions import Self
 
 
 @dataclass
@@ -117,7 +113,7 @@ class ODPNamur:
             self._close_session = True
 
         try:
-            async with async_timeout.timeout(self.request_timeout):
+            async with asyncio.timeout(self.request_timeout):
                 response = await self.session.request(
                     method,
                     url,
